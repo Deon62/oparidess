47d52e2bfbb4da9ccc3dafab030536717ee9368d
Deon62 <orinadeon@gmail.com>
2025-12-17 09:16:55 +0300

    refactored seo

--- metro.config.js
+++ metro.config.js
@@ -3,15 +3,24 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
+let svgTransformerPath;
+try {
+  svgTransformerPath = require.resolve('react-native-svg-transformer');
+} catch (e) {
+  svgTransformerPath = undefined;
+}
 
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
+if (svgTransformerPath) {
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: svgTransformerPath,
+  };
+
+  config.resolver = {
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...config.resolver.sourceExts, 'svg'],
+  };
+}
 
 module.exports = config;
